d363ad5084b8f805521feeff7042bc4dc43983e9
Johnny <johnnyfran20002@gmail.com>
2021-12-13 18:56:37 -0500

    finished

---
+++ src/components/Footer.vue
@@ -0,0 +1,22 @@
+<template>
+  <footer>
+      <p>Copyright &copy; 2021</p>
+      <router-link to="/about">About</router-link> <!--Prevents reload of page-->
+  </footer>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+    a{
+        color: #333
+    }
+    footer{
+        margin-top: 30px;
+        text-align: center;
+    }
+</style>
